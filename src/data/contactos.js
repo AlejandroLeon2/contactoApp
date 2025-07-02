54778744e2a987d3daf6ef3abcbe5735c76b3ed4
Alejandro <alejandroleonpedro7@gmail.com>
2025-07-01 19:30:27 -0500

    corrigiendo y guardando datoslocalstorage

---
+++ src/data/contactos.js
@@ -0,0 +1,35 @@
+ 
+
+export const contacto = [
+    {
+        imagen: "/nice.webp",
+        nombre: "María González",
+        correo: "maria.gonzalez@example.com",
+        numero: "+51 987 654 321"
+    },
+    {
+        imagen: "/nice.webp",
+        nombre: "Luis Rodríguez",
+        correo: "luis.rodriguez@example.com",
+        numero: "+51 912 345 678"
+    },
+    {
+        imagen: "/nice.webp",
+        nombre: "Ana Castillo",
+        correo: "ana.castillo@example.com",
+        numero: "+51 923 456 789"
+    },
+    {
+        imagen: "/nice.webp",
+        nombre: "Carlos Torres",
+        correo: "carlos.torres@example.com",
+        numero: "+51 934 567 890"
+    },
+    {
+        imagen: "/nice.webp",
+        nombre: "Valeria Ríos",
+        correo: "valeria.rios@example.com",
+        numero: "+51 945 678 901"
+    }
+]
+  
